0cbecfc63b86bb790b9fc525a5558962b6207a8c
Masha Soldatova <threebadwolves@gmail.com>
2023-06-25 00:03:16 +0300

    feat: add dependencies

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+/* require('dotenv').config();
 
 const {
   JWT_SECRET = 'JWT_SECRET',
@@ -10,13 +10,12 @@ module.exports = {
   JWT_SECRET,
   DB_ADDRESS,
   NODE_ENV,
-};
+}; */
 
-/*
 const { JWT_SECRET = 'some-secret-key' } = process.env;
 const { DB_ADDRESS = 'mongodb://127.0.0.1:27017/bitfilmsdb' } = process.env;
 
 module.exports = {
   JWT_SECRET,
   DB_ADDRESS,
-}; */
+};
